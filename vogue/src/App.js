90dee0043601599c7f4e66691661d65574c12dd1
Martha Camacho <my.camacho.o@gmail.com>
2021-07-11 13:36:11 -0500

    Add storybook and SASS, and structure folders

---
+++ vogue/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Here we go...</h1>
+      <Button  primary size= 'small' label='Hello!'/>
+    </div>
+  );
+}
+
+export default App;
